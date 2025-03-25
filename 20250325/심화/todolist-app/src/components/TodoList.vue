9ad7687dbbbb6d321b28da1238c9e2902d92106f
hak0622 <seounghak062@naver.com>
2025-03-25 16:51:10 +0900

    심화과제

---
+++ 20250325/심화/todolist-app/src/components/TodoList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <ul class="list-group">
+        <TodoListItem
+          v-for="todo in todolist"
+          :todoitem="todo"
+          @toggle-completed="$emit('toggle-completed', $event)"
+          @delete-todo="$emit('delete-todo', $event)"
+        />
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoListItem from './TodoListItem.vue';
+
+export default {
+  name: 'TodoList',
+  components: { TodoListItem },
+  //부모로 부터 전달 받은 데이터 수신 + 유효성 검사
+  props: {
+    //전달 받은 todolist는 배열 형태, 필수 데이터
+    todolist: { type: Array, required: true },
+  },
+  emits: ['toggle-completed'],
+};
+</script>
