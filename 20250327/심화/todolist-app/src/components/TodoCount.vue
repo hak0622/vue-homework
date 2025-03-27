fc9c181061332d4d1f8af9627904d63f58b08df0
hak0622 <seounghak062@naver.com>
2025-03-27 17:40:31 +0900

    20250327 심화 과제

---
+++ 20250327/심화/todolist-app/src/components/TodoCount.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row mb-3">
+    <div class="col-md text-center">
+      전체 todo 개수 : <span>{{ todoList.length }}</span>
+    </div>
+    <div class="col-md text-center">
+      <!-- 완료 todo 개수 : <span :class="completedCount && completed">{{completedCount}}</span> -->
+      완료 todo 개수 :
+      <span :class="{ completed: completedCount > 0 }">{{
+        completedCount
+      }}</span>
+    </div>
+    <div class="col-md text-center">
+      미완료 todo 개수 :
+      <span :class="{ notCompleted: todoList.length - completedCount > 0 }">{{
+        todoList.length - completedCount
+      }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  //전달 받은 todolist는 배열 형태, 필수 데이터
+  todoList: { type: Array, required: true },
+});
+const completedCount = computed(() => {
+  // todolist의 요소 중 completed 가 true인 요소만 남긴 배열 반환
+  return props.todoList.filter((todo) => todo.completed).length;
+});
+</script>
---
+++ 20250327/심화/todolist-app/src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <ul class="list-group">
+        <TodoListItem
+          v-for="todo in todoList"
+          :todoItem="todo"
+          @toggle-completed="emit('toggle-completed', $event)"
+          @delete-todo="emit('delete-todo', $event)"
+        />
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import TodoListItem from './TodoListItem.vue';
+
+//부모로 부터 전달 받은 데이터 수신 + 유효성 검사
+const props = defineProps({
+  //전달 받은 todolist는 배열 형태, 필수 데이터
+  todoList: { type: Array, required: true },
+});
+const emit = defineEmits(['toggle-completed']);
+</script>
